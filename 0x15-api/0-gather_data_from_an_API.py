fd3d94d1cacc1fc88dfc140b35e51b6395c191fb
uausofteng <uausofteng@gmail.com>
2023-05-22 13:53:14 -0700

    0x15 t

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,38 @@
-#!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+def get_employee_todo_progress(employee_id):
+    # Make a GET request to the API endpoint
+    response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}")
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        todos = response.json()
+        
+        # Filter completed tasks
+        completed_tasks = [todo['title'] for todo in todos if todo['completed']]
+        num_completed_tasks = len(completed_tasks)
+        total_tasks = len(todos)
+        
+        # Get employee name
+        user_response = requests.get(f"https://jsonplaceholder.typicode.com/users/{employee_id}")
+        if user_response.status_code == 200:
+            employee_name = user_response.json()['name']
+        else:
+            print("Failed to retrieve employee name.")
+            return
+        
+        # Display employee TODO list progress
+        print(f"Employee {employee_name} is done with tasks({num_completed_tasks}/{total_tasks}):")
+        for task in completed_tasks:
+            print(f"\t{task}")
+    else:
+        print("Failed to retrieve TODO list.")
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+# Check if the script was provided an employee ID parameter
+if len(sys.argv) < 2:
+    print("Please provide the employee ID as a parameter.")
+else:
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
+
