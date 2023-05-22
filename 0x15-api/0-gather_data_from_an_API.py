1f20fd17c8b8bdcfdf56679bf1d2ed6056d39f1e
uausofteng <uausofteng@gmail.com>
2023-05-22 14:19:10 -0700

    0x15 fixed one

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,6 +10,11 @@ if __name__ == "__main__":
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
     completed = [t.get("title") for t in todos if t.get("completed") is True]
+    total_tasks = len(todos)
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+        user.get("name"), len(completed), total_tasks))
+    
+    for task in todos:
+        if task.get("completed"):
+            print("\t{}".format(task.get("title")))
+
