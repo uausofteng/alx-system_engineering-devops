bdbf455e9b300d1d59e85bbf4ccde8272d565081
uausofteng <uausofteng@gmail.com>
2023-07-24 20:17:18 -0700

    0-gada updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,20 +1,48 @@
 #!/usr/bin/python3
-"""Returns a to-do list information for a given employee ID."""
-import requests
-import sys
+import json
+import urllib.request
 
+def get_employee_todo_list_progress(employee_id):
+    # API URL to get the employee's TODO list
+    url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+
+    try:
+        # Send a GET request to the API
+        with urllib.request.urlopen(url) as response:
+            todo_list = json.loads(response.read())
+
+            # Extract relevant information
+            employee_name = todo_list[0]['name']
+            total_tasks = len(todo_list)
+            done_tasks = sum(1 for task in todo_list if task['completed'])
+            completed_tasks_titles = [task['title'] for task in todo_list if task['completed']]
+
+            # Print the results in the required format
+            print(f"Employee {employee_name} is done with tasks ({done_tasks}/{total_tasks}):")
+            for task_title in completed_tasks_titles:
+                print(f"\t{task_title}")
+
+    except urllib.error.HTTPError as e:
+        print(f"Error: Failed to fetch data. Status code: {e.code}")
+    except urllib.error.URLError as e:
+        print(f"Error: Failed to connect to the API: {e.reason}")
+    except json.JSONDecodeError:
+        print("Error: Failed to parse JSON response from the API.")
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    import sys
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    total_tasks = len(todos)
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), total_tasks))
-    
-    for task in todos:
-        if task.get("completed"):
-            print("\t{}".format(task.get("title")))
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
+
+    try:
+        employee_id = int(sys.argv[1])
+    except ValueError:
+        print("Error: Employee ID should be an integer.")
+        sys.exit(1)
+
+    get_employee_todo_list_progress(employee_id)
 
